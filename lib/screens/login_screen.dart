af0763c1827964aa191704f7308418a478149b78
Ayush Shukla <ayushmw@gmail.com>
2021-08-27 21:09:21 +0530

    updated versions and added log for token

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:developer';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -26,6 +27,7 @@ class LoginScreen extends StatelessWidget {
     // Obtain the auth details from the request
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
+    log(googleAuth.idToken);
 
     // Create a new credential
     final GoogleAuthCredential credential = GoogleAuthProvider.credential(
